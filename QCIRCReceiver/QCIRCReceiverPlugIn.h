65579e2f4ae65b7f68ad8cd7fa80f36bc297527c
Peter Kraml <piets@macpiets.net>
2016-04-17 12:37:12 +0200

    Initial Commit

---
+++ QCIRCReceiver/QCIRCReceiverPlugIn.h
@@ -0,0 +1,17 @@
+//
+//  QCIRCReceiverPlugIn.h
+//  QCIRCReceiver
+//
+//  Created by Peter Kraml on 17.04.16.
+//  Copyright © 2016 MacPietsApps.net. All rights reserved.
+//
+
+#import <Quartz/Quartz.h>
+
+@interface QCIRCReceiverPlugIn : QCPlugIn
+
+// Declare here the properties to be used as input and output ports for the plug-in e.g.
+//@property double inputFoo;
+//@property (copy) NSString* outputBar;
+
+@end
